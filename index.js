a5ae02a71e4cb1eea075f79556e5dd4864a83110
Ayush Bendwal <bendwalayush3@gmail.com>
2021-06-25 21:50:24 +0530

    Graphql done

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import resolvers from './resolvers'
+import schema from './schema'
 
 const app = express()
 
@@ -6,8 +9,18 @@ app.get('/', (req, res) => {
   res.send('Graphql is now the way')
 })
 
-const port = 5000 || process.env.PORT
+const root = resolvers
 
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+    rootValue: root
+  })
+)
+
+const port = 5000 || process.env.PORT
 app.listen(port, () => {
   console.log(`Server is running at: ${port}`)
 })
